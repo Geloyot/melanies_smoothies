69c5f12f52046694f8fda2112db09e82df39c63a
Geloyot <146941312+Geloyot@users.noreply.github.com>
2026-01-30 08:37:34 +0800

    Update streamlit_app.py
    Added a dataframe for selection of fruit options, as well as optimizing the multi-select usage and JSON request syntax for searching of fruit options.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,10 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+pandas_df = my_dataframe.to_pandas()
+st.dataframe(pandas_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients.",
     my_dataframe,
@@ -30,8 +34,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+
+        search_on=pandas_df.loc[pandas_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         snowflake_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
@@ -39,7 +47,7 @@ if ingredients_list:
     my_insert_stmt = """INSERT INTO SMOOTHIES.PUBLIC.ORDERS(ingredients, name_on_order)
             VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-    st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
     
     time_to_insert = st.button('Submit Order')
 
